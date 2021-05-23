559a753e0f76545de8b23e38b35bac4f141fc898
Danny_Yang <r05921014@ntu.edu.tw>
2021-05-23 21:43:47 +0800

    Update wavelet_ssim_loss.py

--- wavelet_ssim_loss.py
+++ wavelet_ssim_loss.py
@@ -16,8 +16,8 @@ class WSloss(nn.Module):
         l, m, h = 1, 1, 1
         for i in range(2):
             l, m, h = l * r * r, l * r * (1 - r), l * (1 - r) * (1 - r)
-            x0, x1, x2 = wt3(x)
-            y0, y1, y2 = wt3(y)
+            x0, x1, x2 = dwt(x)
+            y0, y1, y2 = dwt(y)
             loss = loss - ssim_loss(x1, y1) * 2 * m - ssim_loss(x2, y2) * h
             x, y = x0, y0
         loss -= ssim_loss(x0, y0) * l
